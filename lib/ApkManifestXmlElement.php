7778fd6b1cae5f308ae73dae03a8f4f90c1959e2
Tufan Barış YILDIRIM <tufanbarisyildirim@gmail.com>
2012-03-27 21:38:55 +0300

    Added ApkManifestXmlElement

---
+++ lib/ApkManifestXmlElement.php
@@ -0,0 +1,23 @@
+<?php
+    class ApkManifestXmlElement extends SimpleXMLElement
+    {
+        public function getPermissions()
+        {
+            /**
+            * @var ApkManifestXmlElement
+            */
+            $permsArray = $this->{'uses-permission'};
+            
+            $a = $permsArray->attributes();
+            $perms = array();
+            foreach($permsArray as $perm)
+            {
+                $permAttr = get_object_vars($perm);  
+                $objNotationArray = explode('.',$permAttr['@attributes']['name']);
+                $permName = trim(end($objNotationArray));
+                $perms[$permName] =  ApkManifest::$permissions[$permName];
+            }
+            
+            return $perms;
+        }
+    }
