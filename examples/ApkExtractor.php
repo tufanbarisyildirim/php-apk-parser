32a518d7af74ab72a179975f51f8314ddb5db9dd
Tufan Barış YILDIRIM <tufanbarisyildirim@gmail.com>
2012-03-28 17:31:17 +0300

    assigned "extract_folder" to var.

--- examples/ApkExtractor.php
+++ examples/ApkExtractor.php
@@ -2,11 +2,12 @@
     include '../ApkParser.php';
 
     $apk = new ApkParser('EBHS.apk');
+    $extractFolder = 'extract_folder';
 
-    if(is_dir('extract_folder') || mkdir('extract_folder'))
+    if(is_dir($extractFolder) || mkdir($extractFolder))
     {
-        $apk->getApkArchive()->extractTo('extract_folder');
+        $apk->getApkArchive()->extractTo($extractFolder);
 
         //Change the Manifest XML. @see ApkArchive Todo.
-        file_put_contents('extract_folder/AndroidManifest.xml',$apk->getManifest()->getXmlString());
+        file_put_contents($extractFolder . '/AndroidManifest.xml',$apk->getManifest()->getXmlString());
 }
