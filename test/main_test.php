a53cf3674d1dd4da4de6128e99a0a7c926558982
Tufan Barış YILDIRIM <tufanbarisyildirim@gmail.com>
2012-03-29 14:09:29 +0300

    Written the first test case yeah! - Permissions count, has right perms.

--- test/main_test.php
+++ test/main_test.php
@@ -6,7 +6,24 @@
         {
             public function assertEquals($a,$b)
             {
-                assert($a == $b);
+                return $this->assertTrue($a === $b);
+            } 
+
+            public function assertTrue($exp)
+            {
+                if(assert($exp))
+                {
+                    echo "<span style=\"color:green\">Passed ... </span>\r\n";
+                }   
+                else
+                {
+                    echo "<span style=\"color:red\">Failed ... </span>\r\n";
+                }
+            }
+
+            public function assertHasIndex(array $array, $index)
+            {
+                return  $this->assertTrue(isset($array[$index]));
             } 
         }
     }
@@ -19,5 +36,25 @@
     */
     class Test_Apk_Main extends PHPUnit_Framework_TestCase
     {
-      
+        /**
+        We have 4 permissions in EBHS.apk/AndroidManifest.xml
+        INTERNET
+        CAMERA
+        BLUETOOTH
+        BLUETOOTH_ADMIN
+        */
+        public function TestPermissions()
+        {
+            $apk = new ApkParser('../examples/EBHS.apk');
+            $permissionArray = $apk->getManifest()->getPermissions();
+            $this->assertEquals(count($permissionArray),4);
+            $this->assertHasIndex($permissionArray,'INTERNET');
+            $this->assertHasIndex($permissionArray,'CAMERA');
+            $this->assertHasIndex($permissionArray,'BLUETOOTH');
+            $this->assertHasIndex($permissionArray,'BLUETOOTH_ADMIN');
+        }
     }
+
+    echo '<pre>';
+    $test = new Test_Apk_Main();
+    $test->TestPermissions();
