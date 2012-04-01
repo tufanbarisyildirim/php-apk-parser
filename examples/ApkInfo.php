10e0990096bd50a5cec3f77ed1020f15a3a22b7e
Tufan Barış YILDIRIM <tufanbarisyildirim@gmail.com>
2012-04-01 16:59:57 +0300

    More info on examples/ApkInfo.php

--- examples/ApkInfo.php
+++ examples/ApkInfo.php
@@ -6,6 +6,15 @@
     $permissions = $manifest->getPermissions();
 
     echo '<pre>';
+    echo "Package Name      : " . $manifest->getPackageName()  . "\r\n";
+    echo "Vesrion           : " . $manifest->getVersion()  . "\r\n";
+    echo "Min Sdk Level     : ". $manifest->getMinSdkLevel()  . "\r\n";
+
+    $minPlatform = $manifest->getMinSdk();
+
+    echo "Min Sdk Platfrom  :" . $minPlatform->platform['name'] ."\r\n";
+
+    echo "------------- Permssions List -------------\r\n";
     foreach($permissions as $perm => $description)
     {
         echo $perm . "\t=> " . $description ." \r\n";
