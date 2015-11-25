89f32722138acc108fe827a4979faec03be316b5
Tufan Baris Yildirim <tufanbarisyildirim@gmail.com>
2015-11-25 16:23:46 +0200

    base apk exceptions

---
+++ lib/ApkParser/Exceptions/FileNotFoundException.php
@@ -0,0 +1,7 @@
+<?php
+namespace ApkParser\Exceptions;
+
+class FileNotFoundException extends ApkException
+{
+
+}
---
+++ lib/ApkParser/Exceptions/StreamNotFoundException.php
@@ -0,0 +1,7 @@
+<?php
+namespace ApkParser\Exceptions;
+
+class StreamNotFoundException extends ApkException
+{
+
+}
