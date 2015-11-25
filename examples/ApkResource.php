8976109a9401f038f9c2d22d75d41e90f549b82d
Tufan Baris Yildirim <tufanbarisyildirim@gmail.com>
2015-11-25 15:37:24 +0200

    label example

--- examples/ApkResource.php
+++ examples/ApkResource.php
@@ -14,7 +14,12 @@ $apk = new \ApkParser\Parser('EBHS.apk');
 $resourceId = $apk->getManifest()->getApplication()->getIcon();
 $resources = $apk->getResources($resourceId);
 
+$labelResourceId = $apk->getManifest()->getApplication()->getLabel();
+$appLabel = $apk->getResources($labelResourceId);
+echo $appLabel[0];
+
 header('Content-type: text/html');
+echo $appLabel[0] . '<br/>';
 foreach ($resources as $resource) {
     echo '<img src="data:image/png;base64,', base64_encode(stream_get_contents($apk->getStream($resource))), '" />';
 }
