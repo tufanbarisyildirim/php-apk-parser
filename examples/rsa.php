082ead0685482eed42629f8fdd53a983d9b77b80
Tufan Barış YILDIRIM <tufanbarisyildirim@gmail.com>
2021-12-14 11:33:23 +0300

    update php version, test dumping RSA

---
+++ examples/rsa.php
@@ -0,0 +1,11 @@
+<?php
+
+$cert_path = 'extract_folder/META-INF/CERT.RSA';
+$cert = file_get_contents('extract_folder/META-INF/CERT.RSA');
+
+$beginpem = "-----BEGIN CERTIFICATE-----\n";
+$endpem = "-----END CERTIFICATE-----\n";
+$cert = $beginpem.$cert.$endpem;
+$c = openssl_x509_parse($cert);
+
+var_dump($c,openssl_error_string());
